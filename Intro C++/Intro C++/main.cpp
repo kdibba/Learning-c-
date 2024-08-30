3bc1c471be305d8da626c647bf8a51dd0228be19
Khadija <kd2732@nyu.edu>
2024-08-29 23:00:11 -0400

    Created main file

---
+++ Intro C++/Intro C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Intro C++
+//
+//  Created by Khadijatou Dibba on 8/29/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
